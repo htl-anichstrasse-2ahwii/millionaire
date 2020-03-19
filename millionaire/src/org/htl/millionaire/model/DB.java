41eba10bb5e381404d7b3f0629509142ffb3d930
albertgreinoecker <albert.greinoecker@gmail.com>
2020-03-19 17:25:56 +0100

    added importer methods

--- millionaire/src/org/htl/millionaire/model/DB.java
+++ millionaire/src/org/htl/millionaire/model/DB.java
@@ -3,7 +3,10 @@ package org.htl.millionaire.model;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.htl.millionaire.utils.PropertyReader;
 
@@ -16,9 +19,45 @@ public class DB {
 		String driver = rd.get("driver");
 		String url = rd.get("url");
 		
-		Class.forName(driver); //e.g. "com.mysql.jdbc.Driver"
+		//Class.forName(driver); //e.g. "com.mysql.jdbc.Driver"
 		conn = DriverManager.getConnection(url, user, password);
 		
+	}
+	
+	private int getLastInsertId() throws SQLException
+	{
+		String sql = "SELECT LAST_INSERT_ID()";
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery(sql);
+		rs.next();
+		int id = rs.getInt(1);
+		rs.close();
+		stmt.close();
+		return id;
+	}
+	
+	public int addQuestion(String text, int difficulty, String info, int genre) throws SQLException
+	{
+		String sql = "INSERT INTO question (text, difficulty, info, genre) VALUES (?,?,?,?)";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setString(1, text);
+		stmt.setInt(2, difficulty);
+		stmt.setString(3, info);
+		stmt.setInt(4, genre);
+		stmt.executeUpdate();
+		stmt.close();
+		return getLastInsertId();
+	}
+	
+	public void addAnswer(String text, int questionId, boolean correct) throws SQLException
+	{
+		String sql = "INSERT INTO answer(question_id, text, correct) VALUES (?,?,?)";
+		PreparedStatement stmt = conn.prepareStatement(sql);
+		stmt.setInt(1, questionId);
+		stmt.setString(2, text);
+		stmt.setBoolean(3, correct);
+		stmt.executeUpdate();
+		stmt.close();
 	}
 	
 	public void close() throws SQLException
---
+++ millionaire/src/org/htl/millionaire/model/Importer.java
@@ -0,0 +1,49 @@
+package org.htl.millionaire.model;
+
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Importer {
+
+	public Importer() {
+	}
+
+	public void doImport(String file, int genreId) throws IOException, SQLException, ClassNotFoundException {
+		DB db = new DB();
+		Scanner s = new Scanner (new File(file));
+		s.nextLine(); //skip headline
+		while (s.hasNextLine())
+		{
+			String l = s.nextLine();
+			String[] p = l.split("\t");
+			System.out.println(Arrays.toString(p));
+			int difficulty = Integer.parseInt(p[0]);
+			String qText = p[1];
+			String ans1 = p[2];
+			String ans2 = p[3];
+			String ans3 = p[4];
+			String ans4 = p[5];
+			
+			String info = "";
+			if (p.length == 7)
+				info = p[6];
+			
+			int qId = db.addQuestion(qText, difficulty, info, genreId);
+			db.addAnswer(ans1, qId, true);
+			db.addAnswer(ans2, qId, false);
+			db.addAnswer(ans3, qId, false);
+			db.addAnswer(ans4, qId, false);
+		}
+		s.close();
+		db.close();
+	}
+
+	public static void main(String[] args) throws IOException, SQLException, ClassNotFoundException {
+		Importer importer = new Importer();
+		importer.doImport("data/MillionaireDefaultUS.txt", 1 );
+		
+	}
+}

91895ac0541fa2ff69ffcf27e1c62ea6ee980874
albertgreinoecker <albert.greinoecker@gmail.com>
2020-03-19 10:30:05 +0100

    initial database model

---
+++ millionaire/data/create.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE millionaire;
+USE millionaire;
+
+CREATE TABLE genre
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	label VARCHAR(256) NOT NULL
+);
+
+CREATE TABLE question
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	text VARCHAR(2048) NOT NULL,
+	difficulty INT NOT NULL,
+	info  VARCHAR(2048) NULL,
+	genre INT NOT NULL,
+	FOREIGN KEY (genre) REFERENCES genre(id)
+);
+
+CREATE TABLE answer
+(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	question_id INT NOT NULL,
+	text VARCHAR(2048) NOT NULL,
+	correct BOOL,
+	FOREIGN KEY (question_id) REFERENCES question(id)
+)

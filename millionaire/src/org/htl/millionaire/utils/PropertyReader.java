b1d121b75725a1b875ca103727d1f23a12a1563a
albertgreinoecker <albert.greinoecker@gmail.com>
2020-03-19 13:22:48 +0100

    helper class for reading property files

---
+++ millionaire/src/org/htl/millionaire/utils/PropertyReader.java
@@ -0,0 +1,18 @@
+package org.htl.millionaire.utils;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class PropertyReader {
+	private Properties p;
+
+	public PropertyReader(String path) throws IOException {
+		p = new Properties();
+		p.load(getClass().getClassLoader().getResourceAsStream(path));
+	}
+	
+	public String get(String key)
+	{
+		return p.getProperty(key);
+	}
+}
